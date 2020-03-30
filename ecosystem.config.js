290205258c633003ba6ac2563945cbd7657f98b6
Kadhir Mani <kadhirvelm@gmail.com>
2020-03-30 00:41:57 -0700

    Stop stringify

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
       watch: false,
       node_args: "--experimental-modules --es-module-specifier-resolution=node",
       env_production: {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        PRODUCTION_HOST: JSON.stringify(process.env.PRODUCTION_HOST)
+        NODE_ENV: process.env.NODE_ENV,
+        PRODUCTION_HOST: process.env.PRODUCTION_HOST
       }
     }],
 };
