21b663222391dcd98c956af0bb1b64db5780cbb5
Kadhir Mani <kadhirvelm@gmail.com>
2020-03-30 01:09:37 -0700

    Change production port

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,2 +1,2 @@
-export const PORT = process.env.NODE_ENV === "production" ? 80 : 3000;
+export const PORT = process.env.NODE_ENV === "production" ? 3001 : 3000;
 export const ORIGIN = process.env.NODE_ENV === "production" ? process.env.PRODUCTION_HOST : "localhost";
