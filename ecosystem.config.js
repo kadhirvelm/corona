7f95cb60d74fdccc90fc6546ce006b06be05be52
Kadhir Mani <kadhirvelm@gmail.com>
2020-03-30 00:22:47 -0700

    Try moving things out

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+      name: "corona-service",
+      script: "./dist/index.js",
+      autorestart: false,
+      watch: false,
+      node_args: "--experimental-modules --es-module-specifier-resolution=node",
+      env_production: {
+        NODE_ENV: process.env.NODE_ENV,
+        PRODUCTION_HOST: process.env.PRODUCTION_HOST
+      }
+    }],
+};
