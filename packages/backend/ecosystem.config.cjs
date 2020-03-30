7c4369c95a0ca1cfa3e984f925ec8d5048e63377
Kadhir Mani <kadhirvelm@gmail.com>
2020-03-30 00:12:54 -0700

    More prayers

--- packages/backend/ecosystem.config.cjs
+++ packages/backend/ecosystem.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   apps : [{
     name: "corona-service",
-    script: "dist/index.js",
+    script: "./dist/index.js",
     autorestart: false,
     watch: false,
-    node_args: "esm --experimental-modules --es-module-specifier-resolution=node",
+    node_args: "--experimental-modules --es-module-specifier-resolution=node",
     env_production: {
       NODE_ENV: process.env.NODE_ENV,
       PRODUCTION_HOST: process.env.PRODUCTION_HOST
