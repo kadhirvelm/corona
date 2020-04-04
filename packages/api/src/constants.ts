1bc566026642091a33923eb6e51620f1135942a0
Kadhir Mani <kadhirvelm@gmail.com>
2020-04-04 03:55:08 -0700

    Reset dev settings

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,2 +1,2 @@
 export const PORT = process.env.NODE_ENV === "production" ? 80 : 3000;
-export const ORIGIN = process.env.NODE_ENV === "production" ? process.env.PRODUCTION_HOST : "192.168.0.28";
+export const ORIGIN = process.env.NODE_ENV === "production" ? process.env.PRODUCTION_HOST : "0.0.0.0";
